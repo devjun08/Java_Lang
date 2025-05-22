5c7fff1f336a202790336c776114467125abfbf0
KimJunYeop <165234014+devjun08@users.noreply.github.com>
2025-05-22 22:25:31 +0900

    Add files via upload

---
+++ Java_Introductory/comment.java
@@ -0,0 +1,12 @@
+public class comment {
+    public static void main(String[] args) {
+        // comment
+
+        /*
+        comment
+        comment2
+        */
+
+        System.out.println("Hello! Comment");
+    }
+}
